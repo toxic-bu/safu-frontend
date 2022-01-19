40d1f9e586395c13ef61c06505927549a3aee2c5
toxic-bu <ohmyggma1l@gmail.com>
2022-01-19 13:58:35 +0300

    wallet info

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -24,25 +24,25 @@ const AppNavbar = () => {
 
     return (
         <nav className="flex justify-between items-center md:pt-2 md:px-20 pt-1">
-            <div className="fixed bg-white rounded-full px-2 w-24 flex justify-between  left-2/4 -translate-x-1/2 md:top-3 top-full -translate-y-12 md:translate-y-0 mf:translate-y-2 shadow-white shadow-lg">
+            <div className="fixed bg-white rounded-full px-2 w-24 flex justify-between  left-2/4 -translate-x-1/2 md:top-3 top-full -translate-y-12 md:translate-y-0 mf:translate-y-2 shadow-slate-500 shadow-md">
                 <AppNavbarTab link="/app/info" label="Info" activeTab={activeTab} setActive={setActiveTab} />
                 <AppNavbarTab link="/app/stake" label="Stake" activeTab={activeTab} setActive={setActiveTab} />
             </div>
 
-            <div className="z-10 pl-1">
+            <div className="z-10 pl-1 ">
                 <Link to="/">
                     <img src={logo} alt="logo" className="w-8 mf:w-12 cursor-pointer " />
                 </Link>
             </div>
             {currentAccount ? (
-                <div className="bg-[#fff] px-3 border-2 border-white rounded-full cursor-pointer hover:bg-[#C3FBD8] text-black text-sm mr-2">
+                <div className="bg-[#fff] px-3 border-2 border-white rounded-full cursor-pointer hover:bg-[#C3FBD8] text-black text-sm mr-2 shadow-slate-500 shadow-md">
                     {currentAccount.substr(0, 5)}. .{currentAccount.substr(-5, 5)}
                 </div>
             ) : (
                 <button
                     type="button"
                     onClick={connectWallet}
-                    className="bg-[#fff] px-3 border-2 border-white rounded-full cursor-pointer hover:bg-[#C3FBD8] text-black text-sm mr-2"
+                    className="bg-[#fff] px-3 border-2 border-white rounded-full cursor-pointer hover:bg-[#C3FBD8] text-black text-sm mr-2 shadow-slate-500 shadow-md"
                 >
                     Connect wallet
                 </button>
--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-
+import React from "react";
 import { AppNavbar } from "../components";
 
 const AppPage = () => {
     return (
-        <div className="h-screen gradient-bg-welcome flex flex-col">
-            <div className="h-full blue-bg">
+        <div className="min-h-screen gradient-bg-welcome flex flex-col">
+            <div className="h-screen blue-bg">
                 <AppNavbar />
                 <div className="m-6 md:m-20 flex flex-col items-center ">
                     <Outlet></Outlet>
