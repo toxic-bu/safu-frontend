a7410fb6e003abe0d0e6da0c7800b844527765bf
toxic-bu <ohmyggma1l@gmail.com>
2022-01-23 12:54:26 +0300

    dark theme

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -2,17 +2,18 @@ import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeModeContext";
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
+
 const DarkModeSwitch = () => {
     const { theme, setTheme } = useContext(ThemeContext);
     return (
         <>
             {theme === "dark" ? (
                 <div className="p-2" onClick={() => setTheme("")}>
-                    <MdDarkMode />
+                    <MdLightMode />
                 </div>
             ) : (
                 <div className="p-2" onClick={() => setTheme("dark")}>
-                    <MdLightMode />
+                    <MdDarkMode />
                 </div>
             )}
         </>
--- src/context/ThemeModeContext.jsx
+++ src/context/ThemeModeContext.jsx
@@ -1,9 +1,9 @@
-import { useState, createContext } from "react";
-
+import { createContext } from "react";
+import { useLocalStorage } from "../utils/useLocalStorage";
 export const ThemeContext = createContext();
 
 export const ThemeContextProvider = ({ children }) => {
-    const [theme, setTheme] = useState("");
+    const [theme, setTheme] = useLocalStorage("theme", "dark");
 
     return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
 };
