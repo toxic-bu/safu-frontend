20ceddabb7ff593e7880663a922a2d65bbdcd11b
toxic-by <78828580+toxic-by@users.noreply.github.com>
2022-01-23 20:01:48 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
 export const contractABI = abi.abi;
-export const contractAdress = "0x09994f9922C0F83b0335b6A0850DF4372c338678";
+export const contractAdress = "0x52c004F08e8F09CDca4b900034e4838F3a2F5c7d";
